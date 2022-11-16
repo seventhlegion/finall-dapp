d139ba56877b25a92f96dbc6b9ee5853bd48b0ee
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-11-16 11:55:00 +0330

    tailwind removed

--- src/components/InjectedConnector.card.tsx/index.tsx
+++ src/components/InjectedConnector.card.tsx/index.tsx
@@ -1,6 +1,7 @@
 import { Stack } from '@mui/material';
 import { useAtom } from 'jotai';
 import { ReactElement } from 'react';
+import { MuiStackSx } from '../../theme/theme';
 import { initialBalance } from '../../utils/initialValues';
 import Account from '../Account';
 import Active from '../Active';
@@ -25,14 +26,14 @@ function InjectedConnector({
         <>
             <Stack direction="column" justifyContent="center" alignItems="center" height="full" width="full">
                 <Background />
-                <Stack spacing={2} width={"42rem"} padding={4} marginY={2} className='backdrop-blur-sm shadow-lg'>
+                <Stack spacing={2} width={"42rem"} padding={4} marginY={2} sx={MuiStackSx}>
                     <Stack direction="row" justifyContent="start" alignItems="center" spacing={2} marginY={2} width="full" bgcolor={"none"}>
                         <h1 className='text-xl text-slate-300 font-extrabold'>
                             Wallet
                         </h1>
                         <Active active={active} />
                     </Stack>
-                    <Stack spacing={2} paddingY={2} className='backdrop-blur-sm shadow-lg'>
+                    <Stack spacing={2} paddingY={2} sx={MuiStackSx}>
                         <Owner account={account} />
                         <ChainId chainId={chainId} />
                         <Balance library={library} account={account} token={token as any} chainId={chainId} />
@@ -42,7 +43,7 @@ function InjectedConnector({
                             Transaction
                         </h1>
                     </Stack>
-                    <Stack spacing={2} padding={2} zIndex={10} className='backdrop-blur-sm shadow-lg'>
+                    <Stack spacing={2} padding={2} zIndex={10} sx={MuiStackSx}>
                         <Account token={token} setToken={setToken} account={account} chain={chain} library={library} />
                     </Stack>
                     <Stack direction="row" justifyContent="start" alignItems="start" width="100%" bgcolor={"none"}>
