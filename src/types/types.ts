bbcd2a3d2858b635bb4d85b2eb507e695ffc1c48
Ali Ahmadi <ali.ahmadi.1377@hotmail.com>
2022-11-16 12:34:17 +0330

    networkContract and networkDetails types added

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,36 @@ export type Token = {
     label: string | undefined,
     isCoin: boolean | undefined
 }
+
+export type Networks = {
+    network: {
+        method: string;
+        params: {
+            chainId: string;
+            chainName: string;
+            nativeCurrency: {
+                name: string;
+                symbol: string;
+                decimals: number;
+            };
+            rpcUrls: string[];
+            blockExplorerUrls: string[];
+        }[];
+    };
+    chainId: string;
+    style: string;
+}[]
+
+export type NetworkContracts = {
+    networkChainId: string;
+    networkTokenContracts: ({
+        label: string;
+        isCoin: boolean;
+    }
+        |
+    {
+        value: string;
+        label: string;
+        isCoin: boolean;
+    })[];
+}[] 
